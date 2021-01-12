6fad627b38c519c95d7fd0b460659c4038ad4f10
Block <BEN.BLOCK@tetratech.com>
2021-01-12 09:21:34 -0500

    v1.0.901
    Changed layout of ui.R by putting all tabs in their own script file in the "external" folder

---
+++ inst/shiny-examples/MassIBItools/external/tab_Background.R
@@ -0,0 +1,9 @@
+function(){
+  tabPanel("Background",
+
+           img(src = "MassDEPlogo.png"),
+           includeHTML("App_Background.html"),
+           img(src = "figure1_alt.png", height = 580, width = 1000)
+
+  ) #tabPanel ~END
+}##FUNCTION~END
---
+++ inst/shiny-examples/MassIBItools/external/tab_Instructions.R
@@ -0,0 +1,8 @@
+function(){
+  tabPanel("Instructions",
+
+           img(src = "MassDEPlogo.png"),
+           includeHTML("App_Instructions.html")
+
+  ) #tabPanel ~END
+}##FUNCTION~END
