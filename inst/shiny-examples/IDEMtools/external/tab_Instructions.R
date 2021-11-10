9f51028742827132c795281438ef1fcb73c7fe54
Blocktt <68349347+Blocktt@users.noreply.github.com>
2021-11-10 16:36:56 -0500

    v0.1.2.9003
    Updated instructions tab language

--- inst/shiny-examples/IDEMtools/external/tab_Instructions.R
+++ inst/shiny-examples/IDEMtools/external/tab_Instructions.R
@@ -2,6 +2,7 @@ function(){
   tabPanel("Instructions",
            mainPanel(
              img(src = "IDEM_logo.png", height = 175)
+             ,br()
              ,includeHTML("www/App_Instructions.html")
 
            )##mainPanel~END
